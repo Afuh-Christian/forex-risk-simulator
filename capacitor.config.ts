b0ba6eb4245d74188e61880b4532a2881556f200
Afuh-Christian <afuhchristian10@gmail.com>
2025-11-04 07:43:59 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Forex risk simulator',
+  webDir: 'dist'
+};
+
+export default config;
